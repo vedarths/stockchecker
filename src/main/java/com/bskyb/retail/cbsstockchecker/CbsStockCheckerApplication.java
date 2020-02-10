d591d9c9be0eb538bc7e0d1baeb8db1fdef713f6
skr03 <sudarshan.krishnakumar@sky.uk>
2020-02-10 21:54:00 +0000

    |first commit with spring boot|

---
+++ src/main/java/com/bskyb/retail/cbsstockchecker/CbsStockCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.bskyb.retail.cbsstockchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsStockCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsStockCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bskyb/retail/cbsstockchecker/CbsStockCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bskyb.retail.cbsstockchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbsStockCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
